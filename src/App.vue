0c1f5123fd0196ecc6944aea000dda6526a7cec5
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-10-05 11:31:56 +0200

    Add App.vue & storedResources[]

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource">
+        {{ resource }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+            id:'official-guide',
+            title: 'Official Guide',
+            description: 'The Official Vue.js documentation',
+            link: 'https://vuejs.org'
+        },
+        {
+            id:'google',
+            title: 'Google',
+            description: 'Learn Vue from Google',
+            link: 'https://google.org'
+        }
+      ],
+    };
+  },
+};
+</script>
