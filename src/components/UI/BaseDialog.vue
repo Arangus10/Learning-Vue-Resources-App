31138c6cb864ec249e7c69d7ad08587ec6a57d25
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2023-10-10 20:55:21 +0200

    Combine BaseDialog & AddResources

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,29 +1,32 @@
 <template>
-    <div></div>
-    <dialog open>
-        <header>
-            <slot name="header">
-                <h2>{{ title }}</h2>
-            </slot>
-        </header>
-        <section>
-            <slot></slot>
-        </section>
-        <menu>
-            <slot name="actions"></slot>
-        </menu>
-    </dialog>
+  <div @click="$emit('close')"></div>
+  <dialog open>
+    <header>
+      <slot name="header">
+        <h2>{{ title }}</h2>
+      </slot>
+    </header>
+    <section>
+      <slot></slot>
+    </section>
+    <menu>
+      <slot name="actions">
+        <base-button @click="$emit('close')">Close</base-button>
+      </slot>
+    </menu>
+  </dialog>
 </template>
 
 <script>
 export default {
-    props: {
-        title: {
-            type: String,
-            required: false
-        }
-    }
-}
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+  },
+  emits: ['close']
+};
 </script>
 
 <style scoped>
